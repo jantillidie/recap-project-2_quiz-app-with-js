4279c0b3701dc36aa5ac1f12e8a694806ecc33f6
Jan Maaß <git@antipost.de>
2023-11-03 14:36:49 +0100

    Change text of answer button

--- js/buttonAnswer.js
+++ js/buttonAnswer.js
@@ -5,6 +5,12 @@ const answerParagraph = document.querySelector('[data-js="card-answer"]');
 
 function clickAnswerButton(event) {
   answerParagraph.classList.toggle("card__answer--active");
+
+  if (answerParagraph.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
 };
 
 answerButton.addEventListener("click", clickAnswerButton);
